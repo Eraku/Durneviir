b06c8a087f499b3068944a88284ba595626b2fb6
Eraku Ieta <Thatley3@comcast.net>
2013-02-22 13:05:38 -0700

    First test

---
+++ src/edu/wpi/first3729/frc2013/inputs/ControllerInterlink.java
@@ -0,0 +1,54 @@
+/*  ______ ______ ______ ______
+ * |__    |      |__    |  __  |
+ * |__    |_     |    __|__    |
+ * |______| |____|______|______|
+ */
+package edu.wpi.first3729.frc2013.inputs;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first3729.frc2013.utilities.*;
+
+/**
+ *
+ * @author teddy
+ */
+public class ControllerInterlink extends Input {
+    
+    //! Left and right stick
+    private static final int LEFT_STICK = 0;
+    private static final int RIGHT_STICK = 1;
+    
+    public ControllerInterlink(int number) {
+        super(number);
+    }
+    
+    public double get_x() {
+        return Utility.expo(Utility.normalize(super.get_x(), Params.XMIN, Params.XMAX), Params.XEXPO) * -1.0;
+    }
+
+    public double get_y() {
+         return Utility.expo(Utility.normalize(super.get_y(), Params.YMIN, Params.YMAX), Params.YEXPO);
+    }
+
+    public double get_z() {
+         return 0;
+    }
+
+    public double get_w() {
+         return 0;
+    }
+    
+    public void left_stick() {
+        this._joy0.setAxisChannel(Joystick.AxisType.kX, LEFT_STICK);
+        this._joy0.setAxisChannel(Joystick.AxisType.kY, LEFT_STICK);
+        this._joy0.setAxisChannel(Joystick.AxisType.kZ, LEFT_STICK);
+        this._joy0.setAxisChannel(Joystick.AxisType.kThrottle, LEFT_STICK);
+    }
+    
+    public void right_stick() {
+        this._joy1.setAxisChannel(Joystick.AxisType.kX, RIGHT_STICK);
+        this._joy1.setAxisChannel(Joystick.AxisType.kY, RIGHT_STICK);
+        this._joy1.setAxisChannel(Joystick.AxisType.kZ, RIGHT_STICK);
+        this._joy1.setAxisChannel(Joystick.AxisType.kThrottle, RIGHT_STICK);
+    }
+}
