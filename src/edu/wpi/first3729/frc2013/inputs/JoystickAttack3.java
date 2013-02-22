8e91693792e7784052ea6026a0527b07ed14a874
Eraku Ieta <Thatley3@comcast.net>
2013-02-22 13:05:30 -0700

    First test

---
+++ src/edu/wpi/first3729/frc2013/inputs/JoystickAttack3.java
@@ -0,0 +1,24 @@
+/*  ______ ______ ______ ______
+ * |__    |      |__    |  __  |
+ * |__    |_     |    __|__    |
+ * |______| |____|______|______|
+ */
+package edu.wpi.first3729.frc2013.inputs;
+
+/**
+ *
+ * @author teddy
+ */
+public class JoystickAttack3 extends Input {
+    public JoystickAttack3(int number) {
+        super(number);
+    }
+
+    public double get_z() {
+        return 0.0;
+    }
+
+    public double get_w() {
+        return 0.0;
+    }
+} 
