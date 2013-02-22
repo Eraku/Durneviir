6beddcc5cba43ab4744d7a166b800e25c4f2811f
Eraku Ieta <Thatley3@comcast.net>
2013-02-22 13:05:15 -0700

    First test

---
+++ src/edu/wpi/first3729/frc2013/utilities/Utility.java
@@ -0,0 +1,86 @@
+/*  ______ ______ ______ ______
+ * |__    |      |__    |  __  |
+ * |__    |_     |    __|__    |
+ * |______| |____|______|______|
+ */
+package edu.wpi.first3729.frc2013.utilities;
+
+/**
+ *
+ * @author teddy
+ */
+
+    /**
+     * @brief Clamp a value between a minimum and maximum
+     *
+     * @author Morgan Jones
+     * @param value The value to clamp
+     * @param min The minimum value to allow
+     * @param max The maximum value to allow
+     * @return The clamped value, in [min, max]
+     */
+public class Utility {
+     public static double clamp(double value, double min, double max) {
+        if (value < min) {
+            return min;
+        } else if (value > max) {
+            return max;
+        }
+        return value;
+    }
+
+    /**
+     * @brief Normalize input between -1.0 and 1.0. Also take into account min
+     * and max values given by controller.
+     *
+     * @author Adam Bryant
+     * @param val Input value to be normalized
+     * @param min Minimum controller value. If using a joystick, supply -1.0.
+     * @param max Maximum controller value. If using a joystick, supply 1.0.
+     * @return The normalized value
+     */
+    public static double normalize(double val, double min, double max) {
+        double retVal = 0.0;
+        if (val < 0.0) {
+            retVal = Math.abs(val) / min;
+        } else if (val > 0.0) {
+            retVal = Math.abs(val) / max;
+        }
+        return clamp(retVal, -1.0, 1.0);
+    }
+
+    /**
+     * @brief Convert a boolean to an integer
+     *
+     * @param val The boolean to convert
+     * @return An integer-ized boolean
+     */
+    public static int to_int(boolean val) {
+        if (val) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    public static boolean to_boolean(int val) {
+        if (val != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @brief Exponential function that makes the controller more sensitive
+     * toward the center and less so toward the outside
+     *
+     * @author Adam Bryant
+     * @param x Input to be expo-ed
+     * @param a Pre-defined exponentiation factor
+     * @return The expo-ed value
+     */
+    public static double expo(double x, double a) {
+        return (a * (x * x * x) + (1 - a) * x);
+    }
+}
