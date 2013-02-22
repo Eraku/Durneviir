992b3b1b46b79bea334e5317ccd9ea04f9c97b8e
Eraku Ieta <Thatley3@comcast.net>
2013-02-22 13:05:53 -0700

    First test

---
+++ src/edu/wpi/first3729/frc2013/Gamemode/Disabled.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first3729.frc2013.Gamemode;
+
+import edu.wpi.first3729.frc2013.Robot;
+
+/**
+ *
+ * @author teddy
+ */
+public class Disabled extends GameMode {
+
+    Disabled(Robot robot) {
+        super(robot);
+    }
+    //do absolutley nothing
+    
+}
