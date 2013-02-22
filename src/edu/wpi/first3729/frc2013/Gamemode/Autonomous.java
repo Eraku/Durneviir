62a056cb877f514e38a95872c8663937b86053cb
Eraku Ieta <Thatley3@comcast.net>
2013-02-22 13:05:47 -0700

    First test

---
+++ src/edu/wpi/first3729/frc2013/Gamemode/Autonomous.java
@@ -0,0 +1,20 @@
+/*  ______ ______ ______ ______
+ * |__    |      |__    |  __  |
+ * |__    |_     |    __|__    |
+ * |______| |____|______|______|
+ */
+package edu.wpi.first3729.frc2013.Gamemode;
+
+import edu.wpi.first3729.frc2013.Robot;
+
+/**
+ *
+ * @author teddy
+ */
+public class Autonomous extends GameMode {
+
+    Autonomous(Robot robot) {
+        super(robot);
+    }
+    //Will shoot three frisbees then move forward ish
+}
