13976b18035b306c39b01b95cbe7d5387c3649fe
Eraku Ieta <Thatley3@comcast.net>
2013-03-16 12:00:24 -0600

    Final GameMode transition

--- src/edu/wpi/first3729/frc2013/Robot.java
+++ src/edu/wpi/first3729/frc2013/Robot.java
@@ -34,10 +34,17 @@ public class Robot extends IterativeRobot {
     private Input input_manager;
     private DriverStationLCD screen;
     private Drive drive;
+    private GameMode _mode;
     private Teleoperated teleop;
     private Autonomous auto;
-//    private Manipulator manip;
+//    private Climber climb;
+//    private Shooter shoot;
     private AxisCamera camera;
+    private DriverStationLCD ds = DriverStationLCD.getInstance();
+    /* Line 1 = initalizing reports then current Game mode
+     * Line 2 = current Drive mode
+     * Line 3 = current Shooter speed - TODO
+     */
     
     /**
      * This function is run when the robot is first started up and should be
@@ -45,181 +52,62 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
         // Initialization timer
-        Timer init_timer = new Timer();
-        
+        Timer setuptimer = new Timer();       
         // Print banner
         System.out.println(" ______ ______ ______ ______\n|__    |      |__    |  __  |\n|__    |_     |    __|__    |\n|______| |____|______|______|\n");
- 
-//        // For the lulz
-//        System.out.println("This robot complies with Asimov's Laws of Robotics:");
-//        System.out.println("\t~> 1. A robot may not injure a human being or,\n\t      through inaction, allow a human being to come to harm.");
-//        System.out.println("\t~> 2. A robot must obey the orders given to it by human beings,\n\t      except where such orders would conflict with the First Law.");
-//        System.out.println("\t~> 3. A robot must protect its own existence as long as\n\t      such protection does not conflict with the First or Second Laws.");
-        
+        // For the lulz
+        System.out.println("This robot complies with Asimov's Laws of Robotics:");
+        System.out.println("\t~> 1. A robot may not injure a human being or,\n\t      through inaction, allow a human being to come to harm.");
+        System.out.println("\t~> 2. A robot must obey the orders given to it by human beings,\n\t      except where such orders would conflict with the First Law.");
+        System.out.println("\t~> 3. A robot must protect its own existence as long as\n\t      such protection does not conflict with the First or Second Laws.");
         // Initialize stuff
-        System.out.println("=== INITIALIZING ROBOT ===");
-        
+        ds.println(DriverStationLCD.Line.kUser1, 1,"=== INITIALIZING ROBOT ===");       
         // Start timer
-        init_timer.start();
+        setuptimer.start();
         this.input_manager = new Input();
-        this.input_manager.setmode(Params.default_drive_mode);
+        this.input_manager.setdrivemode(Params.default_drive_mode);
         this.drive = new Drive();
         this.drive.locked();
         this.teleop = new Teleoperated(input_manager, drive, null);
         this.auto = new Autonomous(drive, camera, null);
         this.camera = AxisCamera.getInstance(Params.cameraIP);
         this.camera.writeResolution(Params.cameraresolution);
-        this.camera.writeMaxFPS(Params.cameraFPS);
-        
+        this.camera.writeMaxFPS(Params.cameraFPS);       
         // Set up Watchdog
-        this.getWatchdog().setExpiration(Params.default_watchdog_time);
-        
+        this.getWatchdog().setExpiration(Params.default_watchdog_time);        
         // Stop timer
-        init_timer.stop();
-        
-        System.out.println("=== DONE IN " + init_timer.get() * .001 + " MS ===");
-
+        setuptimer.stop();      
+        ds.println(DriverStationLCD.Line.kUser2, 1,"=== DONE IN " + setuptimer.get() * .001 + " MS ===");
     }
 
-public void disabledInit() {
-        System.out.println("Going disabled.");
+    public void disabledInit() {
+        ds.println(DriverStationLCD.Line.kUser1, 1,"Going disabled.");
+        this._mode = GameMode.todisabled(this._mode, this);
     }
-
     public void disabledPeriodic() {
         // Nothing
     }
 
     public void teleopInit() {
-        System.out.println("Going teleoperated.");
-        teleop.init();
+        ds.println(DriverStationLCD.Line.kUser1, 1,"Going teleoperated.");
+        this._mode = GameMode.toteleoperated(this._mode, this);
+        this._mode.setup();
     }
-
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
         this.getWatchdog().feed();
-        teleop.run();
-    }
-
-    public void teleopContinuous() {
+        this.teleop.run();
     }
     
-    public void autonomousInit()
-    {
-        System.out.println("Going autonomous.");
-        auto.init();
-    }
-    
-    public void autonomousPeriodic()
-    {
+    public void autonomousInit() {
+        ds.println(DriverStationLCD.Line.kUser1, 1,"Going autonomous.");
+        this._mode = GameMode.toautonomous(this._mode, this);
+        this._mode.setup();
+    }   
+    public void autonomousPeriodic() {
         this.getWatchdog().feed();
-        auto.run();
+        this.auto.run();
     }
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    
-}
-//public class Robot extends IterativeRobot {
-    
-//    private GameMode _mode = null;
-    
-//    private Timer auto_timer;
-    
-//    private int state = 0;
 
-    /**
-     * This function is run when the robot is first started up
-     */
-//    public void robotInit() {
-//        // Initialization timer
-//       // Timer init_timer = new Timer();
-//        
-//        // Print banner
-//        System.out.println(" ______ ______ ______ ______\n|__    |      |__    |  __  |\n|__    |_     |    __|__    |\n|______| |____|______|______|\n");
-// 
-//        // For the lulz
-//        System.out.println("This robot complies with Asimov's Laws of Robotics:");
-//        System.out.println("\t~> 1. A robot may not injure a human being or,\n\t      through inaction, allow a human being to come to harm.");
-//        System.out.println("\t~> 2. A robot must obey the orders given to it by human beings,\n\t      except where such orders would conflict with the First Law.");
-//        System.out.println("\t~> 3. A robot must protect its own existence as long as\n\t      such protection does not conflict with the First or Second Laws.");
-//    }
-//
-//    public void disabledInit() {
-//        System.out.println("Going disabled.");
-//        
-//        // Disable the watchdog, because we don't need it
-//        this.getWatchdog().setEnabled(false);
-//        
-//        // Go disabled
-//        this._mode = GameMode.to_disabled(this._mode, this);
-//    }
-//
-//    public void disabledPeriodic() {
-//        this._mode.loop_periodic();
-//    }
-//    
-//    public void disabledContinuous() {
-//        this._mode.loop_continuous();
-//    }
-//
-//    public void teleopInit() {
-//        System.out.println("Going teleoperated.");
-//        
-//        // WAtchdog expiration
-//        this.getWatchdog().setExpiration(5);
-//        
-//        // Enable the watchdog
-//        this.getWatchdog().setEnabled(true);
-//        
-//        // Go teleoperated
-//        this._mode = FRCGameMode.to_teleoperated(this._mode, this);
-//    }
-//
-//    /**
-//     * This function is called periodically during teleoperated mode
-//     */
-//    public void teleopPeriodic() {
-//        // Run one loop
-//        this._mode.loop_periodic();
-//        
-//        // Feed the watchdog
-//        this.getWatchdog().feed();
-//    }
-//
-//    public void teleopContinuous() {
-//        this._mode.loop_continuous();
-//    }
-//    
-//    public void autonomousInit() {
-//        System.out.println("Going autonomous.");
-//
-//        auto_timer = new Timer();
-//        
-//        // Up watchdog expiration for autonomous
-//        this.getWatchdog().setExpiration(15);
-//        
-//        // Disable the watchdog
-//        this.getWatchdog().setEnabled(false);
-//        
-//        // Initialize autonomous
-//        this._mode = FRCGameMode.to_autonomous(this._mode, this);
-//        this._mode.init();
-//    }
-//    
-//    public void autonomousPeriodic() {
-//        // Run a loop
-//        this._mode.loop_periodic();
-//        
-//        // Feed the watchdog
-//        //this.getWatchdog().feed();
-//    }
-//    
-//    public void autonomousContinuous() {
-//        this._mode.loop_continuous();
-//    }
-//}
+    public void testPeriodic() {
+    } 
+}
